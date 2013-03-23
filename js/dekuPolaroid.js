370faca24d392c3fb924e91f608a2aaed9c61a7f
Jorge Blom-Dahl <bommox@gmail.com>
2013-03-23 17:02:01 +0100

    step-2

--- js/dekuPolaroid.js
+++ js/dekuPolaroid.js
@@ -1,79 +1,144 @@
 //Creates the stage
-var dim = new Object({
+var isReady = false;
+
+var dekuPolaroid = new Object({
 		width : 300,
 		height : 350,
 		padding : 13,
 		picHeight : 250,
-		picRadius: 2
+		picRadius: 2,
+		container: 'canvas-container',
+		text : 'SAMPLE TITLE\nAnd sample content!',
+		layer : {},
+		element : {},
+		_state : 0,
+		create : function() {
+			this._prepareCanvas();
+			this._prepareLayers();
+			this._prepareElements();
+			this.layer.bg.add(this.element.bg);
+			this.layer.bg.add(this.element.text);
+			this.layer.pic.add(this.element.picture);	
+			this._loadImages();
+			
+		},
+		setPicture : function(img) {
+			this.element.picture.setFillPatternImage(img);
+			this.element.picture.setFillPriority('pattern');
+			this.layer.pic.draw();
+			this.stage.add(dekuPolaroid.layer.pic);
+			this.layer.shadow.moveToTop();
+		},
+		_prepareCanvas : function() {
+			this.stage = new Kinetic.Stage({
+			container: this.container,
+			width: this.width,
+			height: this.height
+			});
+			var oldClass = this.stage.getContent().getAttribute("class");
+			this.stage.getContent().setAttribute("class", oldClass + " shadowed");
+		},
+		_prepareLayers : function() {
+			this.layer.bg = new Kinetic.Layer();	
+			this.layer.pic = new Kinetic.Layer({
+				x: this.padding,
+				y: this.padding,
+				width: this.stage.getWidth() - this.padding*2,
+				height: this.picHeight,
+			});
+			this.layer.shadow = new Kinetic.Layer({
+				x: this.padding,
+				y: this.padding,
+				width: this.stage.getWidth() - this.padding*2,
+				height: this.picHeight,
+			});
+		},
+		_prepareElements : function() {
+			this.element.text = new Kinetic.Text({
+				x: 0,
+				y: this.picHeight + this.padding,
+				text: this.text,
+				fontSize: 18,
+				fontFamily: 'Shadows Into Light Two',
+				fontStyle: 'bold',
+				fill: '#555',
+				width: this.width,
+				padding: this.padding,
+				align: 'center'
+			});			
+			this.element.bg = new Kinetic.Rect({
+				x:0,
+				y:0,
+				width: dekuPolaroid.stage.getWidth(),
+				height: dekuPolaroid.stage.getHeight(),
+				fill: 'white',
+				strokeWidth: 0
+			});
+			this.element.picture = new Kinetic.Rect({
+				x: 0,
+				y: 0,
+				width: this.layer.pic.getWidth(),
+				height: this.layer.pic.getHeight(),
+				cornerRadius: this.picRadius,
+				fillPatternScale: [0.3,0.3],
+				fillPatternX: 0,
+				fillPatternY: 0
+			});
+		},
+		_loadImages : function() {
+			var imageBg = new Image();
+			var imageShadow = new Image();
+			imageBg._this = this;
+			imageShadow._this = this;
+			imageBg.onload = function() {
+				this._this.element.bg.setFillPatternImage(this);
+				this._this.element.bg.setFillPriority('pattern');
+				this._this.layer.bg.draw();
+				this._this._checkState();
+			};
+			imageShadow.onload = function() {
+				this._this._createShadow(imageShadow);
+				this._this._checkState();
+			};
+			imageBg.src = '../img/polaroid-bg.jpg';
+			imageShadow.src = '../img/shadow.png';
+		},
+		_createShadow: function(img) {
+			this.element.shadow = new Kinetic.Image({
+				x: 0,
+				y:0,
+				width: this.layer.shadow.getWidth(),
+				height: this.layer.shadow.getHeight(),
+				cornerRadius: this.picRadius,
+				image: img,
+				opacity: 0.5
+			});
+			this.layer.shadow.add(this.element.shadow);
+		},		
+		_checkState : function() {
+			this._state += 1;
+			if (this._state == 2) {
+				this.stage.add(dekuPolaroid.layer.bg);
+				this.stage.add(dekuPolaroid.layer.shadow);
+			}
+		}
 	});
 
 
-var stage = new Kinetic.Stage({
-        container: 'canvas-container',
-        width: dim.width,
-        height: dim.height
-    });
-var oldClass = stage.getContent().getAttribute("class");
-stage.getContent().setAttribute("class", oldClass + " shadowed");
-var bgLayer = new Kinetic.Layer();
 
-var shadow = new Kinetic.Rect({
-        x: 0,
-        y: 0,
-        fill: '#555',
-        width: dim.width,
-		height: dim.height
-	});
-	
-var picLayer = new Kinetic.Layer({
-		x: dim.padding,
-		y: dim.padding,
-		width: stage.getWidth() - dim.padding*2,
-		height: dim.picHeight,
-	});
-var textLabel = new Kinetic.Text({
-        x: 0,
-        y: dim.picHeight + dim.padding,
-        text: 'COMPLEX TEXT\nAll the world\'s a stage, and all the men',
-        fontSize: 18,
-        fontFamily: 'Calibri',
-        fill: '#555',
-        width: dim.width,
-        padding: dim.padding,
-        align: 'center'
-    });
-	
-function createImage(){
-	var background = new Kinetic.Rect({
-		x:0,
-		y:0,
-		width: stage.getWidth(),
-		height: stage.getHeight(),
-		fillPatternImage: imageBg,
-        strokeWidth: 0
-	});
-	
-	// add the shape to the layer
-	bgLayer.add(background);		
-	
-	
-	bgLayer.add(textLabel);
-	
-	// add the layer to the stage	
-	stage.add(bgLayer);
-	
-	var o = new Object({x:0, y:0});
+dekuPolaroid.create();
 
-	var picture = new Kinetic.Rect({
-		x: 0,
-		y: 0,
-		width: picLayer.getWidth(),
-		height: picLayer.getHeight(),
-		cornerRadius: dim.picRadius,
-		fillPatternImage: imagePic,
-		fillPatternScale: [0.3,0.3],
-		fillPatternX: 0,
-		fillPatternY: 0,
+
+var imagePic = new Image();
+
+imagePic.onload = function() {
+	dekuPolaroid.setPicture(this);
+};
+
+imagePic.src = '../pic.jpg';
+
+
+		/* ---------------- Not working properly due to shadow layer
 		draggable: true,
 		dragBoundFunc: function(pos) {
 			var a = {x:this.getAbsolutePosition().x, y: this.getAbsolutePosition().y};
@@ -88,33 +153,5 @@ function createImage(){
 				y: this.getAbsolutePosition().y
 			}
 		}
-	});
+		*/
 	
-	picLayer.add(picture);	
-	// apply grayscale filter to second image	
-	stage.add(picLayer);
-	/*
-	picture.applyFilter(Kinetic.Filters.Invert, null, function() {
-	  picLayer.draw();
-	});
-	*/
-}
-
-var loadedPic = 0;
-function checkLoad() {
-	loadedPic++;
-	if (loadedPic == 2) {
-		createImage();
-	}
-}
-
-var imageBg = new Image();
-var imagePic = new Image();
-imageBg.onload = function() {
-	checkLoad() 
-};
-imagePic.onload = function() {
-	checkLoad() 
-};
-imageBg.src = '../img/polaroid-bg.jpg';
-imagePic.src = '../pic.jpg';
